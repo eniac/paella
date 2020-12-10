59d2cd611688ac320f9fd037cebfea0142c100c4
Kelvin Ng <nkwkelvin@gmail.com>
2020-12-10 17:28:22 -0500

    More jobs

---
+++ jobs/tvm_mobilenet/tvm_mobilenet.cpp
@@ -0,0 +1,65 @@
+#include <llis/job/coroutine_job.h>
+#include <llis/job/context.h>
+
+#include <tvm/runtime/module.h>
+#include <tvm/runtime/ndarray.h>
+
+#include <iostream>
+
+class TVMMobilenetJob : public llis::job::CoroutineJob {
+  public:
+    size_t get_input_size() override {
+        return 224*224*3 * sizeof(float);
+    }
+
+    size_t get_output_size() override {
+        return 1000 * sizeof(float);
+    }
+
+    size_t get_param_size() override {
+        return 4;
+    }
+
+    void one_time_init() override {
+        ctx_gpu_ = DLContext{kDLGPU, 0};
+        mod_factory_ = tvm::runtime::Module::LoadFromFile("mobilenetv2-7-cuda_llis-pack.so");
+        gmod_ = mod_factory_.GetFunction("default")(ctx_gpu_);
+        run_ = gmod_.GetFunction("run");
+        tvm::runtime::PackedFunc get_input = gmod_.GetFunction("get_input");
+        tvm::runtime::PackedFunc get_output = gmod_.GetFunction("get_output");
+        input_dev = get_input(0);
+        output_dev = get_output(0);
+    }
+
+    void body(void* io_ptr) override {
+        // TODO: set input, etc
+        set_is_mem();
+        yield();
+        cudaMemcpyAsync(input_dev->data, io_ptr, get_input_size(), cudaMemcpyHostToDevice, get_cuda_stream());
+        unset_is_mem();
+
+        run_();
+
+        set_is_mem();
+        set_pre_notify();
+        yield();
+        cudaMemcpyAsync((char*)io_ptr + get_input_size(), input_dev->data, get_output_size(), cudaMemcpyDeviceToHost, get_cuda_stream());
+    }
+
+  private:
+    DLContext ctx_gpu_;
+    tvm::runtime::Module mod_factory_;
+    tvm::runtime::Module gmod_;
+    tvm::runtime::PackedFunc run_;
+    tvm::runtime::NDArray input_dev;
+    tvm::runtime::NDArray output_dev;
+};
+
+extern "C" {
+
+llis::job::Job* init_job() {
+    return new TVMMobilenetJob();
+}
+
+}
+
