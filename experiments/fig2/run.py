f24ba6780ff884a83eb897e572e84ff28f021674
maxdml <maxdml@seas.upenn.edu>
2023-02-05 00:52:02 -0500

    have the runner script able to iterate through different CUDA_DEVICE_MAX_CONNECTIONS values

--- experiments/fig2/run.py
+++ experiments/fig2/run.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 
+import os
 import subprocess
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('exp_label', type=str)
 parser.add_argument('mode', type=str)
+parser.add_argument('--num_hwq', type=str, default=32)
+parser.add_argument('--iterate-hwq', action='store_true', default=False)
 args = parser.parse_args()
 
-for i in range(1000, 10001, 1000):
-    cmd_args = ['./fig2', args.mode, args.exp_label, str(1e9/i)] # interval in ns
-    p = subprocess.Popen(cmd_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while(1):
-        line = p.stdout.readline()
-        print(line.decode('ascii'))
-        if not line:
-            break
-    p.wait()
+def run_over_load(n_hwq: int):
+    for i in range(1000, 10001, 1000):
+        label = f'{args.exp_label}-{n_hwq}hwq'
+        cmd_args = ['./fig2', args.mode, label, str(1e9/i)] # interval in ns
+        p = subprocess.Popen(cmd_args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=dict(os.environ, CUDA_DEVICE_MAX_CONNECTIONS=str(n_hwq)))
+        while(1):
+            line = p.stdout.readline()
+            print(line.decode('ascii'))
+            if not line:
+                break
+        p.wait()
+
+if (args.iterate_hwq):
+    n_hwq = 1
+    while n_hwq <= 32:
+        run_over_load(n_hwq)
+        n_hwq *= 2
+else:
+    run_over_load(args.num_hwq)
