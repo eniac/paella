d975af4abedfa2ebf25b239d53fec248024ce41c
Kelvin Ng <nkwkelvin@gmail.com>
2023-02-19 15:03:53 -0500

    Update plot script

--- tools/plot_latency_throughput.py
+++ tools/plot_latency_throughput.py
@@ -13,22 +13,32 @@ if __name__ == "__main__":
     parser.add_argument('-a', '--algo_name', dest='algo_names', action='append');
     parser.add_argument('-l', '--line', dest='lines', type=int, action='append');
     parser.add_argument('-n', '--name', dest='names', action='append');
-    parser.add_argument('-x', '--xlim', dest='xlim', type=float);
-    parser.add_argument('-y', '--ylim', dest='ylim', type=float);
+    parser.add_argument('--xlim', dest='xlim', type=float);
+    parser.add_argument('--ylim', dest='ylim', type=float);
+    parser.add_argument('--xaxis', choices=['throughput', 'rate'], type=str);
+    parser.add_argument('--yaxis', dest='yaxises', choices=['mean', 'p50', 'p90', 'p95', 'p99'], action='append', type=str);
 
     args = parser.parse_args()
+
+    yaxis_name2offset = {'mean': 0, 'p50': 1, 'p90': 2, 'p95': 3, 'p99': 4}
 
     num_inputs = len(args.input_paths)
 
     data = [np.genfromtxt(path, delimiter=',') for path in args.input_paths]
 
-    throughputs = [15000. / x[:, 1] * 1000000. for x in data]
+    if args.xaxis == 'throughput':
+        x_axis = [15000. / x[:, 1] * 1000000. for x in data]
+        plt.xlabel('Throughput (req/s)')
+    else:
+        x_axis = [1000000. / x[:, 0] for x in data]
+        plt.xlabel('Sending rate (req/s)')
 
-    rates = [1000000. / x[:, 0] for x in data]
+    plt.ylabel('Latency (us)')
 
     for i in range(num_inputs):
         for line, name in zip(args.lines, args.names):
-            plt.errorbar(throughputs[i], data[i][:, line * 7 + 2], data[i][:, line * 7 + 6 + 2], label=args.algo_names[i] + ' ' + name, fmt='x-', linewidth=1, markersize=2, elinewidth=0)
+            for yaxis in args.yaxises:
+                plt.errorbar(x_axis[i], data[i][:, line * 7 + yaxis_name2offset[yaxis] + 2], data[i][:, line * 7 + 6 + 2], label=args.algo_names[i] + ' ' + name + ' ' + yaxis, fmt='x-', linewidth=1, markersize=2, elinewidth=0)
 
     plt.legend()
     if args.xlim is not None:
