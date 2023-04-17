3bfe41f82e5af9ac7a38dc340ab8cf15ce5eaac8
Kelvin Ng <nkwkelvin@gmail.com>
2023-04-17 15:38:17 -0400

    Update plot script

--- tools/plot_resnet18_inception_v3.sh
+++ tools/plot_resnet18_inception_v3.sh
@@ -8,18 +8,18 @@ python3 $script_path/plot_latency_throughput_subplots.py \
     -o resnet18_inception_v3_prop_newmix3_lns2.pdf \
     -i resnet18_inception_v3_prop_direct_multistream_newmix3_1_lns2.txt \
     -a 'CUDA-MS' \
-    -i resnet18_inception_v3_prop_direct_singlestream_newmix3_2_lns2.txt \
+    -i resnet18_inception_v3_prop_direct_singlestream_newmix3_3_lns2.txt \
     -a 'CUDA-SS' \
-    -i resnet18_inception_v3_prop_singlestream_newmix3_no_mnist_fifo_1_lns2.txt \
+    -i resnet18_inception_v3_prop_singlestream_newmix3_no_mnist_fifo_2_lns2.txt \
     -a 'Paella-SS' \
-    -i resnet18_inception_v3_prop_mps_newmix3_4_lns2.txt \
-    -a 'MPS' \
     -i resnet18_inception_v3_prop_newmix3_fifo_1_lns2.txt \
-    -a 'Paella-CS-jbj' \
+    -a 'Paella-MS-jbj' \
     -i resnet18_inception_v3_prop_newmix3_fifo2_1_lns2.txt \
-    -a 'Paella-CS-kbk' \
+    -a 'Paella-MS-kbk' \
     -i resnet18_inception_v3_prop_newmix3_full3_1_lns2.txt \
     -a 'Paella-SRPT' \
+    -i resnet18_inception_v3_prop_mps_newmix3_4_lns2.txt \
+    -a 'MPS' \
     -m 0 -n All \
     -m 1 -n Resnet18 \
     -m 2 -n InceptionV3 \
@@ -28,24 +28,24 @@ python3 $script_path/plot_latency_throughput_subplots.py \
     --subplotx 1 \
     --subploty 3 \
     --ylim 1000 \
-    --aspect 1
+    --aspect 0.67
 
 python3 $script_path/plot_latency_throughput_subplots.py \
     -o resnet18_inception_v3_prop_newmix3_lns1.5.pdf \
     -i resnet18_inception_v3_prop_direct_multistream_newmix3_1_lns1.5.txt \
     -a 'CUDA-MS' \
-    -i resnet18_inception_v3_prop_direct_singlestream_newmix3_2_lns1.5.txt \
+    -i resnet18_inception_v3_prop_direct_singlestream_newmix3_3_lns1.5.txt \
     -a 'CUDA-SS' \
-    -i resnet18_inception_v3_prop_singlestream_newmix3_no_mnist_fifo_1_lns1.5.txt \
+    -i resnet18_inception_v3_prop_singlestream_newmix3_no_mnist_fifo_2_lns1.5.txt \
     -a 'Paella-SS' \
-    -i resnet18_inception_v3_prop_mps_newmix3_4_lns1.5.txt \
-    -a 'MPS' \
     -i resnet18_inception_v3_prop_newmix3_fifo_1_lns1.5.txt \
-    -a 'Paella-CS-jbj' \
+    -a 'Paella-MS-jbj' \
     -i resnet18_inception_v3_prop_newmix3_fifo2_1_lns1.5.txt \
-    -a 'Paella-CS-kbk' \
+    -a 'Paella-MS-kbk' \
     -i resnet18_inception_v3_prop_newmix3_full3_1_lns1.5.txt \
     -a 'Paella-SRPT' \
+    -i resnet18_inception_v3_prop_mps_newmix3_4_lns1.5.txt \
+    -a 'MPS' \
     -m 0 -n All \
     -m 1 -n Resnet18 \
     -m 2 -n InceptionV3 \
@@ -54,5 +54,5 @@ python3 $script_path/plot_latency_throughput_subplots.py \
     --subplotx 1 \
     --subploty 3 \
     --ylim 1000 \
-    --aspect 1
+    --aspect 0.67
 
