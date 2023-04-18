0c4c3509a4d56f1006048b870277bd6954521b3e
Kelvin Ng <nkwkelvin@gmail.com>
2023-04-17 22:28:38 -0400

    Updatge plot scripts

--- tools/plot_all_no_mnist.sh
+++ tools/plot_all_no_mnist.sh
@@ -6,10 +6,14 @@ cd -
 
 python3 $script_path/plot_latency_throughput_subplots.py \
     -o all_prop_newmix3_no_mnist_lns2.pdf \
-    -i all_prop_direct_multistream_newmix3_no_mnist_1_lns2.txt \
-    -a 'CUDA-MS' \
     -i all_prop_direct_singlestream_newmix3_no_mnist_2_lns2.txt \
     -a 'CUDA-SS' \
+    -i all_prop_direct_multistream_newmix3_no_mnist_1_lns2.txt \
+    -a 'CUDA-MS' \
+    -i /home/maxdml/allis/experiments/clockwork/fig10/2 \
+    -a 'Clockwork' \
+    -i /home/maxdml/triton-client/sosp32_results/2 \
+    -a 'Triton' \
     -i all_prop_singlestream_newmix3_no_mnist_fifo_2_lns2.txt \
     -a 'Paella-SS' \
     -i all_prop_newmix3_no_mnist_fifo_2_lns2.txt \
@@ -17,30 +21,37 @@ python3 $script_path/plot_latency_throughput_subplots.py \
     -i all_prop_newmix3_no_mnist_fifo2_2_lns2.txt \
     -a 'Paella-MS-kbk' \
     -i all_prop_newmix3_no_mnist_full3_2_lns2.txt \
-    -a 'Paella-SRPT' \
-    -i /home/maxdml/triton-client/sosp32_results/2 \
-    -a 'Triton-jbj' \
+    -a 'Paella' \
+    -m 5 -n ResNet-18 \
+    -m 1 -n MobileNetV2 \
+    -m 6 -n ResNet-34 \
+    -m 8 -n SqueezeNet1.1 \
     -m 0 -n All \
-    -m 1 -n Mobilenet \
+    -m 7 -n ResNet-50 \
     -m 2 -n Densenet \
     -m 3 -n GoogleNet \
     -m 4 -n InceptionV3 \
-    -m 5 -n Resnet18 \
-    -m 6 -n Resnet34 \
-    -m 7 -n Resnet50 \
-    -m 8 -n SqueezeNet \
     --xaxis throughput \
     --yaxis p99 \
-    --subplotx 3 \
-    --subploty 3 \
+    --subplotx 2 \
+    --subploty 5 \
+    --legend_subplot 9 \
+    --skip_subplot 9 \
+    --no-xlabel \
+    --width 10.7 \
+    --height 3 \
     --ylim 1000
 
 python3 $1 $script_path/plot_latency_throughput_subplots.py \
     -o all_prop_newmix3_no_mnist_lns1.5.pdf \
-    -i all_prop_direct_multistream_newmix3_no_mnist_1_lns1.5.txt \
-    -a 'CUDA-MS' \
     -i all_prop_direct_singlestream_newmix3_no_mnist_2_lns1.5.txt \
     -a 'CUDA-SS' \
+    -i all_prop_direct_multistream_newmix3_no_mnist_1_lns1.5.txt \
+    -a 'CUDA-MS' \
+    -i /home/maxdml/allis/experiments/clockwork/fig10/1.5 \
+    -a 'Clockwork' \
+    -i /home/maxdml/triton-client/sosp32_results/1.5 \
+    -a 'Triton' \
     -i all_prop_singlestream_newmix3_no_mnist_fifo_2_lns1.5.txt \
     -a 'Paella-SS' \
     -i all_prop_newmix3_no_mnist_fifo_2_lns1.5.txt \
@@ -48,20 +59,22 @@ python3 $1 $script_path/plot_latency_throughput_subplots.py \
     -i all_prop_newmix3_no_mnist_fifo2_2_lns1.5.txt \
     -a 'Paella-MS-kbk' \
     -i all_prop_newmix3_no_mnist_full3_2_lns1.5.txt \
-    -a 'Paella-SRPT' \
-    -i /home/maxdml/triton-client/sosp32_results/1.5 \
-    -a 'Triton-jbj' \
+    -a 'Paella' \
+    -m 5 -n ResNet-18 \
+    -m 1 -n MobileNetV2 \
+    -m 6 -n ResNet-34 \
+    -m 8 -n SqueezeNet1.1 \
     -m 0 -n All \
-    -m 1 -n Mobilenet \
+    -m 7 -n ResNet-50 \
     -m 2 -n Densenet \
     -m 3 -n GoogleNet \
     -m 4 -n InceptionV3 \
-    -m 5 -n Resnet18 \
-    -m 6 -n Resnet34 \
-    -m 7 -n Resnet50 \
-    -m 8 -n SqueezeNet \
     --xaxis throughput \
     --yaxis p99 \
-    --subplotx 3 \
-    --subploty 3 \
+    --subplotx 2 \
+    --subploty 5 \
+    --legend_subplot 9 \
+    --skip_subplot 9 \
+    --width 10.7 \
+    --height 3 \
     --ylim 1000
--- tools/plot_resnet18_inception_v3.sh
+++ tools/plot_resnet18_inception_v3.sh
@@ -6,10 +6,12 @@ cd -
 
 python3 $script_path/plot_latency_throughput_subplots.py \
     -o resnet18_inception_v3_prop_newmix3_lns2.pdf \
-    -i resnet18_inception_v3_prop_direct_multistream_newmix3_1_lns2.txt \
-    -a 'CUDA-MS' \
     -i resnet18_inception_v3_prop_direct_singlestream_newmix3_3_lns2.txt \
     -a 'CUDA-SS' \
+    -i resnet18_inception_v3_prop_direct_multistream_newmix3_1_lns2.txt \
+    -a 'CUDA-MS' \
+    -i resnet18_inception_v3_prop_mps_newmix3_4_lns2.txt \
+    -a 'MPS' \
     -i resnet18_inception_v3_prop_singlestream_newmix3_no_mnist_fifo_2_lns2.txt \
     -a 'Paella-SS' \
     -i resnet18_inception_v3_prop_newmix3_fifo_1_lns2.txt \
@@ -17,25 +19,28 @@ python3 $script_path/plot_latency_throughput_subplots.py \
     -i resnet18_inception_v3_prop_newmix3_fifo2_1_lns2.txt \
     -a 'Paella-MS-kbk' \
     -i resnet18_inception_v3_prop_newmix3_full3_1_lns2.txt \
-    -a 'Paella-SRPT' \
-    -i resnet18_inception_v3_prop_mps_newmix3_4_lns2.txt \
-    -a 'MPS' \
+    -a 'Paella' \
     -m 0 -n All \
-    -m 1 -n Resnet18 \
+    -m 1 -n ResNet-18 \
     -m 2 -n InceptionV3 \
     --xaxis throughput \
     --yaxis p99 \
     --subplotx 1 \
     --subploty 3 \
+    --no-xlabel \
+    --legend_subplot 2 \
     --ylim 1000 \
-    --aspect 0.67
+    --height 1.4
+    #--height 1.6
 
 python3 $script_path/plot_latency_throughput_subplots.py \
     -o resnet18_inception_v3_prop_newmix3_lns1.5.pdf \
-    -i resnet18_inception_v3_prop_direct_multistream_newmix3_1_lns1.5.txt \
-    -a 'CUDA-MS' \
     -i resnet18_inception_v3_prop_direct_singlestream_newmix3_3_lns1.5.txt \
     -a 'CUDA-SS' \
+    -i resnet18_inception_v3_prop_direct_multistream_newmix3_1_lns1.5.txt \
+    -a 'CUDA-MS' \
+    -i resnet18_inception_v3_prop_mps_newmix3_4_lns1.5.txt \
+    -a 'MPS' \
     -i resnet18_inception_v3_prop_singlestream_newmix3_no_mnist_fifo_2_lns1.5.txt \
     -a 'Paella-SS' \
     -i resnet18_inception_v3_prop_newmix3_fifo_1_lns1.5.txt \
@@ -43,16 +48,17 @@ python3 $script_path/plot_latency_throughput_subplots.py \
     -i resnet18_inception_v3_prop_newmix3_fifo2_1_lns1.5.txt \
     -a 'Paella-MS-kbk' \
     -i resnet18_inception_v3_prop_newmix3_full3_1_lns1.5.txt \
-    -a 'Paella-SRPT' \
-    -i resnet18_inception_v3_prop_mps_newmix3_4_lns1.5.txt \
-    -a 'MPS' \
+    -a 'Paella' \
     -m 0 -n All \
-    -m 1 -n Resnet18 \
+    -m 1 -n ResNet-18 \
     -m 2 -n InceptionV3 \
     --xaxis throughput \
     --yaxis p99 \
     --subplotx 1 \
     --subploty 3 \
+    --no-title \
+    --no-legend \
     --ylim 1000 \
-    --aspect 0.67
+    --height 1.4
+    #--height 1.6
 
